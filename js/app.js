c5f4c84cc3dd3b6eaddd24d3a44ebd82513c3a07
Davee Sok <daveesok@gmail.com>
2020-06-09 18:17:21 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let page = 'Page1';
 let sortType = 'horns';
 let dataSource = './data/page-2.json';
 let animalKeywordArr = [];
+let animalArr = [];
 
 function HornedAnimal(animal) {
   this.title = animal.title;
@@ -13,31 +14,13 @@ function HornedAnimal(animal) {
   this.horns = animal.horns;
 }
 
-//code referenced from Class-02 demo folder
-// HornedAnimal.prototype.render = function () {
-//   let $animalClone = $('.photo-template').clone();
-//   $('section').append($animalClone);
-//   $animalClone.find('h2').text(this.title);
-//   $animalClone.find('img').attr('src', this.image_url);
-//   $animalClone.find('alt').attr('alt', this.title);
-//   $animalClone.find('p').text(this.description);
-//   $animalClone.removeClass('photo-template');
-//   $animalClone.attr('class', this.keyword);
-//   console.log($('section'));
-//   console.log('$animalClone :', $animalClone.html());
-// }
-
-//new function for Feature 2 Templating
-
-let animalArr = [];
-
 HornedAnimal.readJson = () => {
   if (page === 'Page1') {
     dataSource = './data/page-1.json';
   } else {
     dataSource = './data/page-2.json';
   }
-  
+
   $.ajax(dataSource)
     .then(data => {
       let keywordsArr = new Set();
@@ -49,14 +32,14 @@ HornedAnimal.readJson = () => {
       render(animalArr);
       generateDropDown(keywordsArr);
     });
-  };
-  console.log(animalArr);
+};
 
-function render (array){
-  array.forEach(animal =>{
-  let $template = $('#template').html();
-  let rendered = Mustache.render($template, animal);
-  $('section').append(rendered);
+
+function render(array) {
+  array.forEach(animal => {
+    let $template = $('#template').html();
+    let rendered = Mustache.render($template, animal);
+    $('section').append(rendered);
   });
 };
 
@@ -67,9 +50,10 @@ function generateDropDown(array) {
   });
 }
 
+
+
 let $dropdown = $('.dropdown');
 $dropdown.on('change', filterKeywords);
-
 
 function filterKeywords() {
   let choice = $(this).val();
@@ -89,8 +73,8 @@ function pageHandler() {
   $('.dropdown').empty();
   animalArr = [];
   $(() => HornedAnimal.readJson());
-
 }
+
 
 let $pageSort = $('.sortOption');
 $pageSort.on('change', sortHandler);
@@ -98,35 +82,35 @@ $pageSort.on('change', sortHandler);
 function sortHandler() {
   sortType = $(this).val();
   $('section').empty();
-    if(sortType === 'horns'){
-      sortHorns(animalArr); 
-    }
-    else if (sortType === 'title'){
-      sortTitle(animalArr);
-    } 
+  if (sortType === 'horns') {
+    sortHorns(animalArr);
+  }
+  else if (sortType === 'title') {
+    sortTitle(animalArr);
+  }
   render(animalArr);
 }
 
-function sortHorns(array){
-  array.sort((a,b) => {
+function sortHorns(array) {
+  array.sort((a, b) => {
     return a.horns - b.horns
   });
 
 }
 
-function sortTitle(array){
-  array.sort((a,b) =>{
+function sortTitle(array) {
+  array.sort((a, b) => {
     var titleA = a.title.toUpperCase();
     var titleB = b.title.toUpperCase();
-    if (titleA < titleB){
+    if (titleA < titleB) {
       return -1;
     }
-    if (titleA > titleB){
+    if (titleA > titleB) {
       return 1;
     }
     return 0;
   });
-console.log(animalArr);
+  console.log(animalArr);
 }
 
 
